6153433b86674a74438d2f1fea5185b3eec5f1d0
Bndy <zb@bndy.net>
2017-07-06 22:46:46 +0800

    py: hello world

---
+++ python/hello.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+browser = webdriver.Firefox()
+
+browser.get('http://www.bing.com/')
+
+elem = browser.find_element_by_id('sb_form_q')
+elem.send_keys('bndy.net' + Keys.RETURN)
+
+WebDriverWait(browser, 10).until(EC.title_is(u'bndy.net - Bing'))
+browser.save_screenshot('~bndy-net.png')
+
+browser.quit()
