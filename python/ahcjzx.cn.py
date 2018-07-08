8a087cbfa77ecabf00d289c75b0d9d5942f5429f
Bendy Zhang <zb@bndy.net>
2018-07-08 20:25:53 +0800

    remove comments

--- python/ahcjzx.cn.py
+++ python/ahcjzx.cn.py
@@ -12,20 +12,19 @@ from selenium.webdriver.support.wait import WebDriverWait
 user = ''
 password = ''
 
-# Maybe you need to set permission for drivers
 isWindows = platform.system() == 'Windows'
 driverPath = '../drivers/geckodriver.exe' if isWindows else '../drivers/geckodriver'
 browser = webdriver.Firefox(executable_path=driverPath)
 baseWindow = browser.current_window_handle
 
-def print_start(action):
-    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S %p') + '    ' + action + '...')
+def print_msg(msg):
+    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S %p') + '    ' + msg)
 
 def print_done():
     print(datetime.now().strftime('%Y-%m-%d %H:%M:%S %p') + '    ' + '完成')
 
 def login():
-    print_start('开始站点登录')
+    print_msg('开始站点登录...')
     browser.get('http://www.ahcjzx.cn/portal/')
 
     elemName = browser.find_element_by_name('userKey')
@@ -38,7 +37,7 @@ def login():
     print_done()
 
 def get_courses():
-    print_start('正在获取课程信息')
+    print_msg('正在获取课程信息...')
     courses = []
     browser.get('http://www.ahcjzx.cn/studentstudio')
     WebDriverWait(browser, 10).until(EC.title_is(u'未结束课程 - 安徽继续教育在线'))
@@ -52,24 +51,21 @@ def get_courses():
         courseIsDone = False
 
         if len(des) > 0 and des[0].get_attribute('title').find('100%') > 0:
-            # skip completed courses
             courseIsDone = True
             continue
 
         courses.append((courseTitle, courseName, courseIsDone, courseUrl))
-    print_done()
     return courses
 
 
 def get_course_lessions(course):
-    print_start(course[0])
+    print_msg(course[0] + '...')
     rootUrl ='http://www.ahcjzx.cn/' + course[3]
     videoLessons = []
     browser.get(rootUrl)
     WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'sh-res')))
 
-    # get all lessions
-    print('============ 剩余课程 ===========') 
+    print('============ 剩余视频课程 ===========') 
     lessons = browser.find_elements_by_class_name('sh-res')
     for lesson in lessons:
         if len(lesson.find_elements_by_class_name('video')) > 0:
@@ -80,23 +76,20 @@ def get_course_lessions(course):
             # in-process: iconfont recent-icon
             # not started: iconfont icon-yuan
             # completed: iconfont icon-iconfontyuan
-            # only video lessons
             if len(lesson.find_elements_by_class_name('icon-iconfontyuan')) > 0:
-                # skip completed items
                 lessonIsDone = True
-            print('    ' + lessonName)
+            else:
+                print('    ' + lessonName)
             videoLessons.append((lessonName, lessonUrl, lessonIsDone))
 
     print_done()
     return videoLessons
 
 def start_learning(course):
-    print('')
-    print('============ 开始学习 ===========') 
     for lesson in course[4]:
         if lesson[2]:
             continue
-        print_start(course[1] + '-' + lesson[0])
+        print_msg(course[1] + ' - ' + lesson[0] + '...')
         browser.get(lesson[1])
         WebDriverWait(browser, 60 * 60).until(EC.presence_of_element_located((By.CLASS_NAME, 'btn-green')))
         print_done()
@@ -108,6 +101,8 @@ for course in courses:
    finalCourses.append(course + (get_course_lessions(course),))
 
 for course in finalCourses:
+    print('')
+    print('============ 开始学习 ===========') 
     if not course[2]:
         start_learning(course)
 
